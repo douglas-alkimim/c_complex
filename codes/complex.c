88fa80a345e08c7ac659e1f4d560c574666cfdde
Douglas de Oliveira Alkimim <douglas.alkimim@outlook.com>
2024-05-15 00:14:03 -0300

    Project Initialization

---
+++ codes/complex.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+struct complex {
+    float real;
+    float imag;
+};
+
+
---
+++ examples/example1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void){
+    printf("%d / %d", sizeof(float), sizeof(double));
+
+    return 0;
+}
